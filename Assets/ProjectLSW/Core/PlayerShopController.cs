798e2da88f60d845a4f521f17a350e882e73d786
Missingnull <felipebarisson@gmail.com>
2023-09-20 15:03:10 -0300

    Add price calculations to buying / selling items

--- Assets/ProjectLSW/Core/PlayerShopController.cs
+++ Assets/ProjectLSW/Core/PlayerShopController.cs
@@ -1,23 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class PlayerShopController : MonoBehaviour
 {
     VisualElement root;
+    Label dialog;
     Button Item0;
     Button Item1;
     Button Item2;
     Button Item3;
 
-    int playerCash = 100;
-    int shopCash = 300;
+    int playerCash = 200;
+    int shopCash = 100;
 
     void Awake()
     {
         // Register inventory and shop items
         root = GetComponent<UIDocument>().rootVisualElement;
+        dialog = root.Q<Label>("Label_Dialog");
         Item0 = root.Q<Button>("VE_Item0");
         Item1 = root.Q<Button>("VE_Item1");
         Item2 = root.Q<Button>("VE_Item2");
@@ -35,17 +38,42 @@ public class PlayerShopController : MonoBehaviour
         Item3.clicked += () => ProcessTransaction(Item3);
     }
 
-    private void ProcessTransaction(VisualElement item)
+    private void ProcessTransaction(Button item)
     {
+        int price = int.Parse(item.text);
+
         // Sell item
         if (item.parent.parent.name == "VE_PlayerItems") // item.(item row).(player/shop inventory)
         {
-            root.Q<GroupBox>("GB_ShopItemsRow1").Insert(0, item);
+            if (price <= shopCash)
+            {
+                shopCash = shopCash - price;
+                playerCash = playerCash + price;
+                UpdateWallets();
+                root.Q<GroupBox>("GB_ShopItemsRow1").Insert(0, item);
+                dialog.text = "Changed your mind, eh? Heh heh";
+            }
+            else
+            {
+                dialog.text = "Sorry, I can't afford that.";
+            }
         }
+
         // Purchase Item
         else if (item.parent.parent.name == "VE_ShopItems")
         {
-            root.Q<GroupBox>("GB_PlayerItemsRow1").Insert(1, item);
+            if (price <= playerCash)
+            {
+                playerCash = playerCash - price;
+                shopCash = shopCash + price;
+                UpdateWallets();
+                root.Q<GroupBox>("GB_PlayerItemsRow1").Insert(1, item);
+                dialog.text = "Heh heh heh thank you!";
+            }
+            else
+            {
+                dialog.text = "Sorry, but you don't seem to have the cash.";
+            }
         }
     }
 
